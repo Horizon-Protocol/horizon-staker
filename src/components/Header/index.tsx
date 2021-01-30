c0ab335ffd83caaf70e5d5c7870709f5965ffed0
lxz <bithub@github.com>
2021-01-30 10:28:01 +0800

    Add header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
+import { Box, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "@assets/logo.png";
+
+const useStyles = makeStyles({
+  container: {
+    padding: "42px 0",
+    borderBottom: "1px solid #11263B",
+  },
+  logo: {
+    height: 40,
+  },
+});
+
 export default function Header() {
-  return <header>Horizon Staker</header>;
+  const classes = useStyles();
+
+  return (
+    <Grid container justify='center' classes={{ container: classes.container }}>
+      <img src={logo} alt='Horizon Mintr' className={classes.logo} />
+    </Grid>
+  );
 }
---
+++ src/utils/theme/index.ts
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: "#120C1C",
+    },
+    secondary: {
+      main: "#62B5DB",
+    },
+  },
+});
+
+export default theme;
