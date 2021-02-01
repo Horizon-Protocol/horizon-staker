d4ddae25a20577bd87d9f63a88a841209e33df84
Steven Li <steven.li@seneceesg.com>
2021-02-01 11:02:40 +0800

    Add network config.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -27,3 +27,28 @@ export const SUPPORTED_WALLETS: Wallet[] = [
     injection: "BinanceChain",
   },
 ];
+
+export enum NETWORK {
+  MAINNET = 56, // BSC
+  TESTNET = 97, // BSC testnet
+}
+
+export const BSC_JSON_RPC_URLS = {
+  [NETWORK.MAINNET]: `https://bsc-dataseed.binance.org/`,
+  [NETWORK.TESTNET]: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+};
+
+export const CONTRACT_ADDRESS = {
+  phb: {
+    [NETWORK.MAINNET]: "",
+    [NETWORK.TESTNET]: "0x171B2B6B6Efc088E3D77a3F5Cc1E0F9C9301F9dD",
+  },
+  hzn: {
+    [NETWORK.MAINNET]: "",
+    [NETWORK.TESTNET]: "0x0B14682818B42fE1859999D159788abCd6f4C96F",
+  },
+  bnb: {
+    [NETWORK.MAINNET]: "",
+    [NETWORK.TESTNET]: "",
+  },
+};
