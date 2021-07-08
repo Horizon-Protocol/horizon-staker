60b4c7acd36bbe1a36fa8398d17ed10230e92e86
lxzxl <bithub@github.com>
2021-07-08 17:30:50 +0800

    feat: add ga.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
+import ReactGA from "react-ga";
 import WalletsDialog from "@components/WalletsDialog";
 import Header from "@components/Header";
 import Home from "./Home";
@@ -15,6 +16,11 @@ const Version = withStyles(() => ({
     color: "#666",
   },
 }))(Typography);
+
+ReactGA.initialize("UA-199967475-1", {
+  // debug: true,
+});
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
